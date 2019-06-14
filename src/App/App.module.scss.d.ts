783af0b0a8fce0e988b1febf61ae92f962bd0de8
blackart <blackart.ru@gmail.com>
2019-06-14 19:33:57 +0800

    add css modules

---
+++ src/App/App.module.scss.d.ts
@@ -0,0 +1,7 @@
+export const main: string;
+export const app: string;
+export const appLogo: string;
+export const appLogoSpin: string;
+export const appHeader: string;
+export const appLink: string;
+export const btn: string;
